7bca2fd4d822de59cfbbcaf4825c28edee649183
kami2k1 <quangtd031@gmail.com>
2024-07-26 10:29:07 +0700

    aaaaaaaaaaa

--- Main.py
+++ Main.py
@@ -18,15 +18,15 @@ headers = {
     "Sec-Fetch-Site": "cross-site",
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36"
 }
-
-response = requests.get(url, headers=headers)
-def kami(response):
-  if response.status_code == 200:  
-    print("Success!")
-    print(response.json())
-kami(response)
-time.sleep(float(sle))
-response = requests.post(url, headers=headers)
-kami(response)
-response = requests.post("https://cat-backend.pro/v1/auth/profile", headers=headers)
-kami(response)
+while True:
+  response = requests.get(url, headers=headers)
+  def kami(response):
+    if response.status_code == 200:  
+      print("Success!")
+      print(response.json())
+  kami(response)
+  time.sleep(float(sle))
+  response = requests.post(url, headers=headers)
+  kami(response)
+  response = requests.post("https://cat-backend.pro/v1/auth/profile", headers=headers)
+  kami(response)
