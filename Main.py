5d12bdc08ba5c67ca2e3fc964594fcbc8c317be3
kami2k1 <quangtd031@gmail.com>
2024-07-28 23:55:08 +0700

    Quang Update

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-import requests , time , threading
+import requests , time , threading , sys
 token = input(str("TOKen You: "))
-sle = input(str("Thời gian Thu Thập tính bằng s: "))
+sle = input(str("Time Thu Thập Tối Thiểu 100s: "))
+if float(sle) <100:
+   print("lỗi : Time >100 ?")
+   sys.exit()
 url = "https://cat-backend.pro/v1/points/mining"
 headers = {
     "Accept": "*/*",
@@ -36,40 +39,46 @@ def MiNIGame():
      response = requests.post("https://cat-backend.pro/v1/games/mines-start", headers=headers,json=data)
      print(f"{response.json()}") 
      time.sleep(3)
-while True:
-  msg =""
-  response = requests.get(url, headers=headers)
-  def kami(response , type) -> None:
-    global msg , stattickce
-    if response.status_code == 200:  
-      print("Success!")
-      kami = response.json()
-      for key, value in kami.items():
-            msg += f"{key} : {value}\n"
-      if int(type) ==1:
-        data =  int(kami['playing_tickets_amount'])
-        if data > 0 and stattickce == False:
-           stattickce = True
-           thread1 = threading.Thread(target=MiNIGame, args=())
-           thread1.start()
-    else:
-      print(response.text)
-  kami(response,2)
-  time.sleep(float(sle))
-  response = requests.post(url, headers=headers)
-  kami(response,2)
-  
-  response = requests.get("https://cat-backend.pro/v1/auth/profile", headers=headers)
-  kami(response,type=1)
-  my = f'''
----------------------------------------------
-                                            
-            Kami
-data:
-{msg}
-
-
-
----------------------------------------------
-'''
-  print(my)
+game_run = True
+while game_run:
+    try:
+        msg =""
+        response = requests.get(url, headers=headers)
+        def kami(response , type) -> None:
+          global msg , stattickce , game_run
+          if response.status_code == 200:  
+            print("Success!")
+            kami = response.json()
+            for key, value in kami.items():
+                  msg += f"{key} : {value}\n"
+            if int(type) ==1:
+              data =  int(kami['playing_tickets_amount'])
+              if data > 0 and stattickce == False:
+                 stattickce = True
+                 thread1 = threading.Thread(target=MiNIGame, args=())
+                 thread1.start()
+          else:
+            game_run = False
+        kami(response,2)
+        time.sleep(float(sle))
+        response = requests.post(url, headers=headers)
+        kami(response,2)
+        
+        response = requests.get("https://cat-backend.pro/v1/auth/profile", headers=headers)
+        kami(response,type=1)
+        my = f'''
+      ---------------------------------------------
+                                                  
+                  Kami
+      data:
+      {msg}
+      
+      
+      
+      ---------------------------------------------
+      '''
+        print(my)
+    except:
+       print("Vui Lòng Kiểm Tra Lại kiết Nối mạng Của Bạn ?")
+       sys.exit()     
+print("Lỗi Token Hoặc Api Của Game Đã Thay đổi Vui Long Kiểm Tra Lại Thank ")
